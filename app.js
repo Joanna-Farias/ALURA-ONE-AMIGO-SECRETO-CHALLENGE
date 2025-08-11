78113ce106dffd9251ea8cef1d184ad364322737
Joanna-Farias <johlm.sf@gmail.com>
2025-08-11 16:04:52 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let friends = [];
+
+function addFriendInput () {
+    let addFriend = document.getElementById('#amigo');
+
+} // Input para adicionar nome dos amigos.
+
+function addFriend() {
+    let buttonAddFriend = document.getElementById('#button-add');
+
+
+} // Botão para adicionar amigos.
+
+function drawFriend(){
+    let buttonDrawFriend = document.getElementById('#button-draw');
+    
+} // Botão para sortear amigo.
+
